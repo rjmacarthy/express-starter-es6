be8ad831fcc97bbe916967b8233096377e361b84
rjmacarthy <richardmacarthy@hotmail.com>
2016-02-22 20:16:01 +0000

    Arrow

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import express from 'express';
-import http  from 'http';
-import { config }  from './config/config';
+import http from 'http';
+import { config } from './config/config';
 import app from './config/express';
 
 var server = http.createServer(app());
 
 server.listen(config.port);
 
-server.on('error', function() {
+server.on('error', () => {
     console.log('error starting server');
 });
 
-server.on('listening', function() {
+server.on('listening', () => {
     console.log('Server started on port ' + config.port);
 });
 
